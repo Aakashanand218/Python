991e6d7fdb17575e18760762f1e2e37c44b6943c
Aakashanand218 <anandaakash218@gmail.com>
2025-05-18 22:22:11 +0530

    new code

--- product of string.py
+++ product of string.py
@@ -1,16 +1,17 @@
 def multiply_strings(s1, s2):
     try:
-        # Convert strings to integers
+        
         num1 = int(s1)
         num2 = int(s2)
-        # Calculate the product
+        
         product = num1 * num2
         return product
     except ValueError:
         return "Invalid input. Please provide valid numeric strings."
 
-# Example usage
+
 s1 = str(input("Enter first number:"))
 
 s2 = str(input("Enter second number:"))
 print("Product:", multiply_strings(s1, s2))
+
